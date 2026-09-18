5d16e0198447e0bfc6189a88f57db072fa9e6600
Ayesha Putri Amira <ayesha170107@gmail.com>
2026-09-18 17:55:58 +0700

    Praktikum 2 - Sintak dasar, variabel & operator

---
+++ Praktikum-2/src/DataPribadi.java
@@ -0,0 +1,15 @@
+public class DataPribadi {
+        public static void main(String[] args) {
+            // Deklarasi variabel
+            int umur = 19;
+            double tinggi = 153.5;
+            char inisial = 'A';
+            boolean mahasiswaAktif = true;
+
+            // Cetak seluruh data
+            System.out.println("Umur: " + umur);
+            System.out.println("Tinggi : " + tinggi);
+            System.out.println("Inisial: " + inisial);
+            System.out.println("Aktif: " + mahasiswaAktif);
+    }
+}
