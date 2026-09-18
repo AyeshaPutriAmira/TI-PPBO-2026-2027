1622855991bab814a0cf82d61b1c5a87ae622d74
Ayesha Putri Amira <ayesha170107@gmail.com>
2026-09-18 17:47:00 +0700

    Praktikum-2: Struktur dasar program java A.2.

--- Praktikum-2/src/HelloWorld.java
+++ Praktikum-2/src/HelloWorld.java
@@ -1,6 +1,12 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Halo, Ayesha!");
+        System.out.print("Nama: ");
+        System.out.print("Ayesha");
+        System.out.println();
+        System.out.println("Ini baris baru");
+
+
 
 
     }
